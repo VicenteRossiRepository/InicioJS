e4f3d740431c48d95a32d8f8a26d423ac13bc934
Vicente <vicente.rossi.contacto@gmail.com>
2024-08-10 20:56:56 -0400

    creacion clases 02.js y 03.js para declaracion de variables let y const

---
+++ js/02.js
@@ -0,0 +1,18 @@
+//Variables con let
+
+let producto = 'Audifonos gamers'; //metodo antiguop para crear variables
+let disponible; //Se puede crear vaiables sin valor
+
+// producto = true; //Se puede cambiar el valor independiente del tipo 
+
+let producto1 = 'Computador',
+    disponible1 = true,
+    categoria = 'computadoras'; //Se pueden crear distintas variables en un solo var
+
+// let _1disponible //Variables no pueden inciar con numeros, guiones
+
+//Estilos variables
+let nombre_producto = 'Monitor HD' //underscore
+let nombreProducto = 'Monitor HD' //Camelcase
+let NombreProducto = 'Monitor HD' //Pascal case
+let nombreproducto = 'Monitor HD' //under case
---
+++ js/03.js
@@ -0,0 +1,18 @@
+//Variables con const
+
+const producto = 'Audifonos gamers'; //metodo antiguop para crear variables
+const disponible = true; //No se puede crear vaiables sin valor
+
+// producto = true; //No se puede cambiar el valor independiente del tipo 
+
+const producto1 = 'Computador',
+    disponible1 = true,
+    categoria = 'computadoras'; //Se pueden crear distintas variables en un solo var
+
+// const _1disponible //Variables no pueden inciar con numeros, guiones
+
+//Estilos variables
+const nombre_producto = 'Monitor HD' //underscore
+const nombreProducto = 'Monitor HD' //Camelcase
+const NombreProducto = 'Monitor HD' //Pascal case
+const nombreproducto = 'Monitor HD' //under case
