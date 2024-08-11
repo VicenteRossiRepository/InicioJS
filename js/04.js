ac88900fdd76ee4f81efe7d4f0027f5469d470ae
Vicente <vicente.rossi.contacto@gmail.com>
2024-08-10 21:03:39 -0400

    creacion clase 04.js de variables tipo string

---
+++ js/04.js
@@ -0,0 +1,11 @@
+//Variables string
+
+const producto = "Monitor 20 pulgadas" //variable String
+const producto2 = String("Monitor 30 pulgadas")
+const producto3 = new String("Monitor 50 pulgadas") 
+const producto4 = "Monitor de 20\"";
+
+console.log(producto)
+console.log(producto2)
+console.log(producto3)
+console.log(producto4)
