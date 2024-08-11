e60e69c192da1b74211c6d64bd6fc2e8a6b015d3
Vicente <vicente.rossi.contacto@gmail.com>
2024-08-10 20:48:30 -0400

    Primer commit

---
+++ js/01.js
@@ -0,0 +1,20 @@
+// Variables
+
+var producto = 'Audifonos gamers'; //metodo antiguop para crear variables
+var disponible; //Se puede crear vaiables sin valor
+
+// producto = true; //Se puede cambiar el valor independiente del tipo 
+
+var producto1 = 'Computador',
+    disponible1 = true,
+    categoria = 'computadoras'; //Se pueden crear distintas variables en un solo var
+
+// var 1disponible //Variables no pueden inciar con numeros, guiones
+
+//Estilos variables
+var nombre_producto = 'Monitor HD' //underscore
+var nombreProducto = 'Monitor HD' //Camelcase
+var NombreProducto = 'Monitor HD' //Pascal case
+var nombreproducto = 'Monitor HD' //under case
+
+console.log(producto)
