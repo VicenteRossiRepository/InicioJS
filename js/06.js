963a3609e56f3a67c3f3b8b2531a441672e0f16e
Vicente <vicente.rossi.contacto@gmail.com>
2024-08-11 20:31:49 -0400

    Creacion clase 06.js de objeto Math y sus funciones

---
+++ js/06.js
@@ -0,0 +1,14 @@
+// Objeto math
+
+let resultado;
+
+resultado = Math.PI //Numero PI
+resultado = Math.round(2.6) //Redondea numero
+resultado = Math.ceil(2.3) //Redondea numero a numero mayor
+resultado = Math.floor(2.9) //Redondea numero a numero menor
+resultado = Math.abs(-144) //Convierte numero positivo a negativo y viceversa
+resultado = Math.min(3,5,1,10) //Devuelve numero minimno de un listado
+resultado = Math.max(3,5,1,10) //Devuleve numero maximo de un listado
+resultado = Math.random() // Devuelve numero aleatorio
+
+console.log(resultado)
