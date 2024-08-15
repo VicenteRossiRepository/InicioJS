c15063daa6759fefc94bb96b5fe556ac19fe9291
Vicente <vicente.rossi.contacto@gmail.com>
2024-08-15 13:04:25 -0400

    Creacion clase 08.js de concatenacion de strings

---
+++ js/08.js
@@ -0,0 +1,9 @@
+const nombre = "vicente"
+const email = "mail@server.com"
+
+// Concatenacion
+console.log(nombre +" "+ email)
+console.log(nombre , email)
+
+// Template string - String literals
+console.log(`Nombre Cliente: ${nombre} Emali: ${email}`)    
