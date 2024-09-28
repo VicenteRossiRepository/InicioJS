de00ce35547f73b3bf158bb128f9d265a80cf653
Vicente <vicente.rossi.contacto@gmail.com>
2024-09-27 21:11:06 -0300

    Creacion clase 16.js de funciones

---
+++ js/16.js
@@ -0,0 +1,16 @@
+// Declaracion de funcion
+function sumar(){
+    console.log(10+10);
+}
+sumar();
+
+// expresion de la funcion
+const sumar2 = function(){
+    console.log(3+3);
+}
+sumar2();
+
+// IIFE
+(function(){
+    console.log("Esto es una funcion")
+})();
