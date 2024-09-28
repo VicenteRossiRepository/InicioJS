bc45724a00bbe7362cf9f5a5932ef3f190b0fd00
Vicente <vicente.rossi.contacto@gmail.com>
2024-09-27 21:19:21 -0300

    Creacion clase 17.js de la diferencia entre funcion y metodo

--- js/16.js
+++ js/16.js
@@ -1,14 +1,17 @@
 // Declaracion de funcion
+sumar();
 function sumar(){
     console.log(10+10);
 }
-sumar();
+
+//Se puede llamar la funcion antes de declararla
 
 // expresion de la funcion
 const sumar2 = function(){
     console.log(3+3);
 }
 sumar2();
+//Primero tiene que declararse la funcion antes de llamarla
 
 // IIFE
 (function(){
---
+++ js/17.js
@@ -0,0 +1,6 @@
+const numero1 = 20;
+const numero2 = "20";
+
+console.log(parseInt(numero2)); //ParseInt() es una funcion por los parentesis y con la variable dentro del ()
+
+console.log(numero1.toString()); //toString() es un metodo por ser una funcion unida a la variable por un "." 
