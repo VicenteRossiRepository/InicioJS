bc4fee427580c71f62981c290ee7e2a810a5d35c
Vicente <vicente.rossi.contacto@gmail.com>
2024-10-28 21:24:58 -0300

    Creacion clase 24.js de loops For

---
+++ js/24.js
@@ -0,0 +1,32 @@
+//For loop
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log("----------------------------------------------------------------")
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(`El número ${i} es PAR`);
+    }else{
+        console.log(`El número ${i} es IMPAR`);
+    }
+}
+
+console.log("----------------------------------------------------------------")
+const carrito = [
+    {nombre:'Monitor 20 pulgadas', precio: 500},
+    {nombre:'Television 50 pulgadas', precio: 700},
+    {nombre:'Tablet', precio: 300},
+    {nombre:'Audifonos', precio: 200},
+    {nombre:'Teclado', precio: 50},
+    {nombre:'Celular', precio: 500},
+    {nombre:'Bocinas', precio: 300},
+    {nombre:'Laptop', precio: 800}
+];
+
+for (let i = 0; i < carrito.length; i++) {
+    console.log(carrito[i].nombre);    
+}
+//while loop
+//do while loop
