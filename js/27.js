cba6d13d836d6757e78235cb5c0c6723f20fcdb5
Vicente <vicente.rossi.contacto@gmail.com>
2024-11-21 21:34:14 -0300

    Modificacion de clase 27.js con creacion de prototipos de clases

--- js/27.js
+++ js/27.js
@@ -13,12 +13,34 @@ function Producto(nombre, precio, disponibilidad) {
     this.disponible = disponibilidad
 }
 
+function Cliente(nombre, apellido){
+    this.nombre = nombre
+    this.apellido = apellido
+}
+
+//Crear funciones que solo se utilizan en objetos en especificos
+Producto.prototype.formatearProducto = function(){
+    return `El producto ${this.nombre} tiene un precio de $${this.precio}`;
+}
+
+Cliente.prototype.getNombreCompleto = function(){
+    return `El cliente ${this.nombre} ${this.apellido}`;
+}
+
 const monitor = new Producto(`Monitor curvo de 48"`, 800, true);
 const laptop = new Producto(`laptop`, 300, false);
 const celular = new Producto(`Telefono celular`, 400, false);
 const procesador = new Producto(`Procesador`, 1000, true);
 
-console.log(monitor);
-console.log(laptop);
+const cliente1 = new Cliente("Juan", "Perez");
+const cliente2 = new Cliente("Rodrigo", "Rojas");
+
+
+
+console.log(monitor.formatearProducto());
+console.log(laptop.formatearProducto());
 console.log(celular);
-console.log(procesador);
+console.log(procesador.formatearProducto());
+
+console.log(cliente1.getNombreCompleto());
+console.log(cliente2);
