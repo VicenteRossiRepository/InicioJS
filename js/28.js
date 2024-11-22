7ea70f009943eb16d44b85908ad639055aec157a
Vicente <vicente.rossi.contacto@gmail.com>
2024-11-21 21:53:57 -0300

    Modificacion de clase 28.js de herencias

--- js/28.js
+++ js/28.js
@@ -18,5 +18,18 @@ class Producto {
 const monitor = new Producto(`Monitor curvo de 48"`, 800);
 const laptop = new Producto(`laptop`, 300);
 
-console.log(laptop.devolverPrecio());
+// Herencia
+class Libro extends Producto{
+    constructor(nombre, precio, isbn){
+        super(nombre, precio);
+        this.isbn = isbn;
+    }
+
+    formaterProducto(){
+        return `${super.formaterProducto()} y su ISBN es ${this.isbn}`;
+    }
+}
+
+const libro = new Libro(`Libro de JavaScript`, 150, '978-84-98-3856-1');
+console.log(libro.formaterProducto());
 console.log(monitor.formaterProducto());
